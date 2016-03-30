686fa7f81406f368aa6335cf20ac0759ca4ae1af
Dan Gaston <daniel.gaston@deaddriftbio.com>
2016-03-30 15:36:39 -0300

    Adjust the output VCF

--- convert_civic_tsv2vcf.py
+++ convert_civic_tsv2vcf.py
@@ -10,11 +10,17 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     with open(args.output, 'w') as output:
+        output.write("##fileformat=VCFv4.2\n")
+        output.write("""##INFO=<ID=CIVIC_VARTYPE,Number=1,Type=String,Description="CiVic Variation Type">\n""")
+        output.write("""##INFO=<ID=CIVIC_INFO,Number=1,Type=String,Description="CiVic Variation Info">\n""")
+        output.write("""##INFO=<ID=CIVIC_VARGROUP,Number=1,Type=String,Description="CiVic Variation Group">\n""")
         with open(args.input, 'r') as infile:
-            reader = csv.reader(infile, dialect='excel-tab')
+            reader = csv.reader(infile, delimiter='\t')
+            reader.next()
             for row in reader:
-                output.write("{chr}\t{pos}\t{ref}\t{alt}\t.\t.\tVARINFO={info}\n".format(chr=row[4],
-                                                                                         pos=row[5],
-                                                                                         ref=row[7],
-                                                                                         alt=row[8],
-                                                                                         info=row[2]))
+                if len(row) >= 10:
+                    if any([row[8], row[9]]):
+                        output.write("{chr}\t{pos}\t{ref}\t{alt}\t.\t.\t"
+                                     "CIVIC_VARTYPE='{type}';CIVIC_INFO='{info}'\t"
+                                     "CIVIC_VARGROUP='{group}'\n".format(chr=row[5], pos=row[6], ref=row[8], alt=row[9],
+                                                                         type=row[2], info=row[3], group=row[4]))
