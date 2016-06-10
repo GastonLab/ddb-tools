f2d9c11941dc107f8840bece77af9b49aca70f0e
Dan Gaston <daniel.gaston@deaddriftbio.com>
2016-06-10 10:45:57 -0300

    attempting a weird bugf fix

--- fixFasta.py
+++ fixFasta.py
@@ -16,8 +16,8 @@ if __name__ == "__main__":
     with open(args.input, 'r') as alignfh:
         alignment = AlignIO.parse(alignfh, 'fasta')
 
-    sys.stdout.write("Writing FASTA to file {}\n".format(args.output))
-    with open(args.output, 'w') as outfh:
-        AlignIO.write(alignment, outfh, 'fasta')
+        sys.stdout.write("Writing FASTA to file {}\n".format(args.output))
+        with open(args.output, 'w') as outfh:
+            AlignIO.write(alignment, outfh, 'fasta')
 
     sys.stdout.write("Finished!\n")
