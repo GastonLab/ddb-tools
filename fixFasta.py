9eb4b877a64377cbe27a449b9f42c2b90b84faf5
Dan Gaston <daniel.gaston@deaddriftbio.com>
2016-06-10 10:36:23 -0300

    Adding file to fix FASTA format files for cuffmerge

---
+++ fixFasta.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+import argparse
+from Bio import AlignIO
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', help="Input FASTA filename")
+    parser.add_argument('-o', '--output', help="Fixed output FASTA filename")
+    args = parser.parse_args()
+    args.logLevel = "INFO"
+
+    sys.stdout.write("Reading input file {}\n".format(args.input))
+    with open(args.input, 'r') as alignfh:
+        alignment = AlignIO.parse(alignfh, 'fasta')
+
+    sys.stdout.write("Writing FASTA to file {}\n".format(args.output))
+    with open(args.output, 'w') as out_alignfh:
+        AlignIO.write(alignment, out_alignfh, 'fasta')
+
+    sys.stdout.write("Finished!\n")
