6dcfbde2eba3e40b6a51c487ff9455339730d432
Dan Gaston <daniel.gaston@gmail.com>
2016-12-09 15:39:45 -0400

    Working on hgvs conversion script

--- hgvs_to_genomic.py
+++ hgvs_to_genomic.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 
 import argparse
+import pyhgvs as hgvs
+import hgvs.utils as hgvs_utils
+
+from pygr.seqdb import SequenceFileDB
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--infile', help="Input file")
+    parser.add_argument('-r', '--ref', help="Genome reference FASTA")
     parser.add_argument('-o', '--outfile', help="Output file name")
+
+    args = parser.parse_args()
+
+    genome = SequenceFileDB(args.ref)
