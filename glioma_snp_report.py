ec70a692e0c5a21ad0ddc01e1d8ea538a810ec16
Daniel Gaston <daniel.gaston@dal.ca>
2019-04-11 10:09:33 -0300

    Writing summaryd ata instead of individual data

--- glioma_snp_report.py
+++ glioma_snp_report.py
@@ -9,21 +9,28 @@ from ddb import configuration
 from collections import defaultdict
 
 
+def write_data():
+    with open("{}.snp_freqs.txt".format(samples[sample]['library_name']),
+              "w") as report:
+        report.write("Chrom\tStart\tStop\tSNP ID\tSomatic AF\tDepth\tCallers\n")
+
+
 def process_sample(parse_functions, sample, samples, config, snp_list):
     caller_records = defaultdict(lambda: dict())
+    snp_data = defaultdict(lambda: defaultdict)
 
     sys.stdout.write("Parsing Caller VCF Files\n")
-    vcf_parsing.parse_vcf("{}.mutect.normalized.vcf".format(sample),
+    vcf_parsing.parse_vcf("{}.mutect.normalized.vcf.gz".format(sample),
                           "mutect", caller_records)
-    vcf_parsing.parse_vcf("{}.vardict.normalized.vcf".format(sample),
+    vcf_parsing.parse_vcf("{}.vardict.normalized.vcf.gz".format(sample),
                           "vardict", caller_records)
-    vcf_parsing.parse_vcf("{}.freebayes.normalized.vcf".format(sample),
+    vcf_parsing.parse_vcf("{}.freebayes.normalized.vcf.gz".format(sample),
                           "freebayes", caller_records)
-    vcf_parsing.parse_vcf("{}.scalpel.normalized.vcf".format(sample),
+    vcf_parsing.parse_vcf("{}.scalpel.normalized.vcf.gz".format(sample),
                           "scalpel", caller_records)
-    vcf_parsing.parse_vcf("{}.platypus.normalized.vcf".format(sample),
+    vcf_parsing.parse_vcf("{}.platypus.normalized.vcf.gz".format(sample),
                           "platypus", caller_records)
-    vcf_parsing.parse_vcf("{}.pindel.normalized.vcf".format(sample),
+    vcf_parsing.parse_vcf("{}.pindel.normalized.vcf.gz".format(sample),
                           "pindel", caller_records)
 
     annotated_vcf = "{}.vcfanno.snpEff.GRCh37.75.vcf".format(sample)
@@ -32,50 +39,35 @@ def process_sample(parse_functions, sample, samples, config, snp_list):
     vcf = VCF(annotated_vcf)
 
     sys.stdout.write("Processing individual variants\n")
-    written_snp_count = 0
-    written_snps = list()
-    with open("{}.snp_freqs.txt".format(samples[sample]['library_name']),
-              "w") as report:
-        report.write("Chrom\tStart\tStop\tSNP ID\tSomatic AF\tDepth\tCallers\n")
-        for variant in vcf:
-            if variant.ID in snp_list:
-                written_snp_count += 1
-                written_snps.append(variant.ID)
-                # Parsing VCF and creating data structures for Cassandra model
-                callers = variant.INFO.get('CALLERS').split(',')
 
-                key = (unicode("chr{}".format(variant.CHROM)), int(variant.start),
-                       int(variant.end), unicode(variant.REF), unicode(variant.ALT[0]))
+    for variant in vcf:
+        if variant.ID in snp_list:
+            # Parsing VCF and creating data structures for Cassandra model
+            callers = variant.INFO.get('CALLERS').split(',')
+            key = (unicode("chr{}".format(variant.CHROM)), int(variant.start),
+                   int(variant.end), unicode(variant.REF), unicode(variant.ALT[0]))
 
-                caller_variant_data_dicts = defaultdict(dict)
-                max_som_aaf = -1.00
-                max_depth = -1
-                min_depth = 100000000
+            caller_variant_data_dicts = defaultdict(dict)
+            max_som_aaf = -1.00
+            max_depth = -1
+            min_depth = 100000000
 
-                for caller in callers:
-                    caller_variant_data_dicts[caller] = parse_functions[caller](caller_records[caller][key])
-                    if float(caller_variant_data_dicts[caller]['AAF']) > max_som_aaf:
-                        max_som_aaf = float(caller_variant_data_dicts[caller]['AAF'])
-                    if int(caller_variant_data_dicts[caller]['DP']) < min_depth:
-                        min_depth = int(caller_variant_data_dicts[caller]['DP'])
-                    if int(caller_variant_data_dicts[caller]['DP']) > max_depth:
-                        max_depth = int(caller_variant_data_dicts[caller]['DP'])
+            for caller in callers:
+                caller_variant_data_dicts[caller] = parse_functions[caller](caller_records[caller][key])
+                if float(caller_variant_data_dicts[caller]['AAF']) > max_som_aaf:
+                    max_som_aaf = float(caller_variant_data_dicts[caller]['AAF'])
+                if int(caller_variant_data_dicts[caller]['DP']) < min_depth:
+                    min_depth = int(caller_variant_data_dicts[caller]['DP'])
+                if int(caller_variant_data_dicts[caller]['DP']) > max_depth:
+                    max_depth = int(caller_variant_data_dicts[caller]['DP'])
 
-                if min_depth == 100000000:
-                    min_depth = -1
+            if min_depth == 100000000:
+                min_depth = -1
 
-                report.write("{}\t{}\t{}\t{}\t{}\t{}\t{}"
-                             "\n".format(variant.CHROM, variant.start,
-                                         variant.end, variant.ID, max_som_aaf,
-                                         max_depth, ",".join(callers)))
-        not_found = 0
-        for snp in snp_list:
-            if snp not in written_snps:
-                not_found += 1
-                report.write(".\t.\t.\t{}\t.\t.\t.\n".format(snp))
-    sys.stdout.write("Variant data for {} SNPS written for sample {}."
-                     " {} SNPs not found.\n".format(written_snp_count,
-                                                    sample, not_found))
+            snp_data[variant.ID]['freq'] = max_som_aaf
+            snp_data[variant.ID]['depth'] = max_depth
+
+    return snp_data
 
 
 if __name__ == "__main__":
@@ -102,10 +94,24 @@ if __name__ == "__main__":
                        'platypus': vcf_parsing.parse_platypus_vcf_record,
                        'pindel': vcf_parsing.parse_pindel_vcf_record}
     snps = list()
+    sample_snp_data = defaultdict(lambda: defaultdict)
 
     with open(args.list, 'r') as fh:
         snps = [current_snp.rstrip() for current_snp in fh.readlines()]
 
     for sample in samples:
         sys.stdout.write("Processing sample {}\n".format(sample))
-        process_sample(parse_functions, sample, samples, config, snps)
+        sample_snp_data[sample] = process_sample(parse_functions, sample,
+                                                 samples, config, snps)
+
+    sys.stdout.write("Writing out data\n")
+    with open("glioma_snp_data.txt", 'wb') as out:
+        out.write("SNP")
+        for sample in samples:
+            out.write("\t{} - AAF\t{} - Depth".format(sample, sample))
+        out.write("\n")
+        for snp in snps:
+            out.write("{}".format(snp))
+            for sample in samples:
+                out.write("\t{}\t{}".format(sample_snp_data[sample]['freq'],
+                                            sample_snp_data[sample]['depth']))
